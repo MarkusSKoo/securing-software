6c2115fa1f4f4d5540f758069babec9cd7f209ad
MarkusSKoo <markus.kauhanen@helsinki.fi>
2025-04-30 14:23:59 +0300

    Add functionality

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    username TEXT UNIQUE,
+    password_hash TEXT
+);
+
+CREATE TABLE comments (
+    id INTEGER PRIMARY KEY,
+    comment TEXT,
+    user_id INTEGER REFERENCES users,
+    comment TEXT
+);
